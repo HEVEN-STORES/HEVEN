4a0861fc4cd7640b9bcaae144cb339eeaaa83c9b
HEVEN-STORES <heven.storess@gmail.com>
2025-07-05 14:56:39 +0530

    Update server.js

--- backend/server.js
+++ backend/server.js
@@ -1,3 +1,43 @@
+// import express from 'express'
+// import cors from 'cors'
+// import 'dotenv/config' ;
+// import connnectDb from './config/mogodb.js'
+// import connectClouinary from './config/cloudinary.js'
+// import userRouter from './routes/userRoute.js'
+// import productRouter from './routes/productRoute.js';
+// import cartRouter from './routes/cartRoute.js';
+// import orderRouter from './routes/orderRoute.js';
+
+// // APP CONFIG
+
+// const app = express()
+// const port = process.env.PORT || 4000
+// connnectDb()
+// connectClouinary()
+
+// // MiddleWares
+
+// app.use(express.json())
+// app.use(cors())
+
+// // API endPoints
+
+// app.use('/api/user',userRouter)
+// app.use('/api/product',productRouter)
+// app.use('/api/cart',cartRouter)
+// app.use('/api/order',orderRouter)
+
+
+
+// app.get('/',(req,res)=>{
+//         res.send("API WORKING")
+// })
+
+// app.listen(port,()=>console.log('server sarted on PORT :'+port))
+
+
+
+
 import express from 'express'
 import cors from 'cors'
 import 'dotenv/config' ;
@@ -7,6 +47,8 @@ import userRouter from './routes/userRoute.js'
 import productRouter from './routes/productRoute.js';
 import cartRouter from './routes/cartRoute.js';
 import orderRouter from './routes/orderRoute.js';
+import couponRoutes from './routes/coupon.js';
+
 
 // APP CONFIG
 
@@ -15,10 +57,14 @@ const port = process.env.PORT || 4000
 connnectDb()
 connectClouinary()
 
+
+
 // MiddleWares
+app.use('/api/coupon', couponRoutes);
 
 app.use(express.json())
 app.use(cors())
+// app.use('/api/coupon', couponRoutes);
 
 // API endPoints
 
@@ -26,6 +72,9 @@ app.use('/api/user',userRouter)
 app.use('/api/product',productRouter)
 app.use('/api/cart',cartRouter)
 app.use('/api/order',orderRouter)
+
+
+
 
 
 
