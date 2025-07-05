44e82e2fa8193b9cd2c723ed09f3df40151306c8
HEVEN-STORES <heven.storess@gmail.com>
2025-07-05 20:52:16 +0530

    Update productModel.js

--- backend/models/productModel.js
+++ backend/models/productModel.js
@@ -1,8 +1,33 @@
+// import mongoose from "mongoose";
+
+// const productSchema = new mongoose.Schema({
+//     name: { type: String, require: true },
+//     description: { type: String, require: true },
+//     actualPrice: { type: Number, require: true },
+//     price: { type: Number, require: true },
+//     image: { type: Array, require: true },
+//     category: { type: String, require: true },
+//     subCategory: { type: String, require: true },
+//     sizes: { type: Array, require: true },
+//     bestseller: { type: Boolean },
+//     date: { type: Number, require: true },
+
+// })
+
+// const productModel = mongoose.models.product|| mongoose.model("product",productSchema);
+
+// export default productModel
+
+
+
 import mongoose from "mongoose";
 
 const productSchema = new mongoose.Schema({
     name: { type: String, require: true },
     description: { type: String, require: true },
+    type: { type: String, require: true },
+    features: { type: String, require: true },
+    quality: { type: String, require: true },
     actualPrice: { type: Number, require: true },
     price: { type: Number, require: true },
     image: { type: Array, require: true },
