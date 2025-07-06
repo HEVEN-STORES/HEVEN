76cffa7cf66e588d4c2049410ff4f59de005ae44
HEVEN-STORES <heven.storess@gmail.com>
2025-07-06 10:54:36 +0530

    Update Add.jsx

--- admin/src/pages/Add.jsx
+++ admin/src/pages/Add.jsx
@@ -311,8 +311,8 @@ const Add = ({token}) => {
         <div>
             <p  className='mb-2'>Product Category</p>
             <select onChange={(e)=>setCategory(e.target.value)} className='w-full px-3 py-2' >
-              <option value="Unisex">Unisex</option>
               <option value="Men">Men</option>
+              <option value="Unisex">Unisex</option>
               <option value="Women">Women</option>
               <option value="Kids">Kids</option>
               <option value="Footwear">Foot Wear</option>
