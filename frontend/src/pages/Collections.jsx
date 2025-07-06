d3204c41aecc13b13c689d5322e0e92ac2bf402e
HEVEN-STORES <heven.storess@gmail.com>
2025-07-06 10:46:38 +0530

    Update Collections.jsx

--- frontend/src/pages/Collections.jsx
+++ frontend/src/pages/Collections.jsx
@@ -94,6 +94,9 @@ useEffect(()=>{
         <div className={`border border-gray-300 pl-5 py-3 mt-6${showFilter ? '':'hidden'}sm:block`}>
           <p className='mb-3 text-sm font-medium  '>CATEGORIES</p>
           <div className='flex flex-col gap-2 text-sm font-light text-gray-700'>
+             <p className='flex gap-2'>
+              <input type="checkbox" className='w-3' value={'Unisex'}  onChange={toggleCategory}/>Unisex
+            </p>
             <p className='flex gap-2'>
               <input type="checkbox" className='w-3' value={'Men'}  onChange={toggleCategory}/>Men
             </p>
